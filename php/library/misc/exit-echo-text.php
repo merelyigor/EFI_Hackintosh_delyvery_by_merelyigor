5126de0020258f17cecc8fa35fc2ebb27aee0320
Merely_igor_homePC <mogila1708@gmail.com>
2020-10-01 20:36:34 +0300

    Update exit-echo-text.php

--- php/library/misc/exit-echo-text.php
+++ php/library/misc/exit-echo-text.php
@@ -1,6 +1,32 @@
 <?php
-include_once '../My.bash.class.php';
-use My_bash_class\My_bash_class;
+/**
+ * клас для создания bash команд и их выполнения или обработки
+ *
+ * @param string $command = команда на bash
+ *
+ * @return string $bash_return = результат выполнения bash команды
+ * @return string $shell_return = результат выполнения bash команды
+ */
+class My_bash_class
+{
+    public function bash($command)
+    {
+        $shell_return = system($command);
+        return $shell_return;
+    }
+
+    public function EEF($text)
+    {
+        echo "{$text}" . PHP_EOL;
+    }
+
+    public function bash_escapeshellarg($command)
+    {
+        $bash_return = system(escapeshellarg($command));
+        return $bash_return;
+    }
+}
+
 $bash = new My_bash_class();
 $bash->bash("clear");
 $bash->EEF("
